dcd84dc512785077dfcfc97253efc60a88a4f940
bluenex <tulakan.r@gmail.com>
2017-11-29 16:33:31 +0700

    add rpi branch

---
+++ change_servo_mode.py
@@ -0,0 +1,38 @@
+import smbus
+import RPi.GPIO as GPIO
+import time
+
+#  setup button input pins
+BTN_1 = 6
+BTN_2 = 13
+BTN_3 = 19
+BTN_4 = 26
+SOUND_PIN = 5
+MOTOR_PIN = 9
+
+GPIO.setmode(GPIO.BCM)  # set board mode to Broadcom
+GPIO.setup(BTN_1,GPIO.IN)
+GPIO.setup(BTN_2,GPIO.IN)
+GPIO.setup(BTN_3,GPIO.IN)
+GPIO.setup(BTN_4,GPIO.IN)
+GPIO.setup(SOUND_PIN, GPIO.OUT)
+GPIO.output(SOUND_PIN, GPIO.LOW)
+GPIO.setup(MOTOR_PIN, GPIO.OUT)
+GPIO.output(MOTOR_PIN, GPIO.HIGH)
+
+IODIR = 0x00 # I/O DIRECTION REGISTER
+GPIO_ROBOT  = 0x09 # Set/Reset GPIO REGISTER
+output_mode  = 0x00
+
+bus = smbus.SMBus(1)  # I2C port 1
+
+control_byte = 0x21  # control byte not include R/W bit use just 7 bit
+
+def write_output(control_byte, register_address, value):
+        bus.write_byte_data(control_byte, register_address, value)
+
+write_output(control_byte, IODIR, output_mode) # setup output port A
+# WriteOutput(control_byte,GPIOA,0x00) # Write the output to port A
+# time.sleep(3)
+write_output(control_byte, GPIO_ROBOT, 0xFF) # Write the output to port A
+time.sleep(0.5)
--- dxlcli.py
+++ dxlcli.py
@@ -1,168 +1,55 @@
-from pyax12.connection import Connection
+#!/home/pi/miniconda3/bin/python
 
-class MotorManager:
+import sys
+import time
+from ax12 import Ax12
+
+class BasicController:
     '''
-    Class to manage motor 
-    port on Windows should be 'COMX'
-    port on Posix should be '/dev/ttyUSBX'
+    Class to control basic functions of  motor on Rpi.
     '''
-    def __init__(self, p="/dev/ttyUSB0", baudRate=1000000, scanLimit=20, defaultSpeed=90):
-        self.serial_connection = Connection(port=p, baudrate=baudRate)
+    def __init__(self, scanLimit=20, defaultSpeed=90):
+        self.connection = Ax12()
         self.connected_motor_ids = []
         self.scan_limit = scanLimit
         self.default_speed = defaultSpeed
-        self.available_baudrate = '''+------+-----------+------------+
-| Data |  Set BPS  | Target BPS |
-+------+-----------+------------+
-|    1 | 1000000.0 |  1000000.0 |
-|    3 |  500000.0 |   500000.0 |
-|    4 |  400000.0 |   400000.0 |
-|    7 |  250000.0 |   250000.0 |
-|    9 |  200000.0 |   200000.0 |
-|   16 |  117647.1 |   115200.0 |
-|   34 |   57142.9 |    57600.0 |
-|  103 |   19230.8 |    19200.0 |
-|  207 |    9615.4 |     9600.0 |
-+------+-----------+------------+'''
-        self.available_baud_ids = [1, 3, 4, 7, 9, 16, 34, 103, 207]
-
-    def terminateConnection(self):
-        self.serial_connection.close()
-        print("Connection is closed.")
 
     ###########################
     # Get Connected Motor Ids #
     ###########################
 
     def getConnectIds(self):
-        self.connected_motor_ids = list(self.serial_connection.scan(range(self.scan_limit)))
+        self.connected_motor_ids = self.connection.learnServos()
         print("Available ids are", self.connected_motor_ids)
-
-    ###########
-    # Set Ids #
-    ###########
-
-    def setNewIdOf(self, motor_id, new_id):
-        if motor_id == new_id or new_id < 1 or new_id > 254:
-            print("Please use id in range of 1-254 and not duplicated with old id: {0}".format(motor_id))
-            return
-
-        self.serial_connection.set_id(motor_id, new_id)
-        print("Motor id {0} is changed to id {1}.".format(motor_id, new_id))
-
-    #####################
-    # Print Information #
-    #####################
-
-    def printAvailableBaudrates(self):
-        print(self.available_baudrate)
-
-    def printControlTable(self, motor_id):
-        self.serial_connection.pretty_print_control_table(motor_id)
 
     ####################
     # Position Control #
     ####################
 
     def setPositionOf(self, motor_id, pos=512, speed=90):
-        self.serial_connection.goto(motor_id, pos, speed=speed)
-    
+        self.connection.moveSpeed(motor_id, pos, speed=speed)
+
     def setPositionAll(self, pos=512, speed=90):
         for motorId in self.connected_motor_ids:
-            self.serial_connection.goto(motorId, pos, speed)
+            self.connection.moveSpeed(motorId, pos, speed)
 
     def getPositionOf(self, motor_id):
-        return self.serial_connection.get_goal_position(motor_id)
+        return self.connection.readPosition(motor_id)
 
     def getPositionAll(self):
         temp = {}
         for motorId in self.connected_motor_ids:
-            temp[motorId] = self.serial_connection.get_goal_position(motorId)
+            temp[motorId] = self.connection.readPosition(motorId)
 
         return temp
 
     ####################
-    # Baudrate Control #
+    # Torque Control #
     ####################
 
-    def getBaudrateOf(self, motor_id):
-        return self.serial_connection.get_baud_rate(motor_id)
+    def setTorqueOf(self, motor_id, status):
+        self.connection.setTorqueStatus(motor_id, status)
 
-    def getBaudrateAll(self):
-        temp = {}
+    def setTorqueAll(self, status):
         for motorId in self.connected_motor_ids:
-            temp[motorId] = self.serial_connection.get_baud_rate(motorId)
-
-        return temp
-
-    def setBaudrateOf(self, motor_id, baudId=1):
-        # check if input baudId is in range
-        if baudId not in self.available_baud_ids:
-            print("Please use baudId got from 'Data' column below.")
-            self.printAvailableBaudrates()
-        else:
-            self.serial_connection.set_baud_rate(motor_id, baudId, unit="internal")
-            print("Motor id {0} baudrate is set to {1}: {2}.".format(motor_id, baudId, round(2000000. / (baudId + 1))))
-
-    def setBaudrateAll(self, baudId=1):
-        # check if input baudId is in range
-        if baudId not in self.available_baud_ids:
-            print("Please use baudId got from 'Data' column below.")
-            self.printAvailableBaudrates()
-        else:
-            for motorId in self.connected_motor_ids:
-                self.serial_connection.set_baud_rate(motorId, baudId, unit="internal")
-                print("Motor id {0} baudrate is set to {1}: {2}.".format(motorId, baudId, round(2000000. / (baudId + 1))))
-
-    #########
-    # Helps #
-    #########
-
-    @staticmethod
-    def help():
-        txt = '''List of methods available.
-        ====== Connection ======
-        --- Import
-        from dxlcli import MotorManager
-
-        --- Instantiate class
-        m = MotorManager(p="/dev/ttyUSB0", baudRate=1000000, scanLimit=20, defaultSpeed=90)
-        *** USE COMXX on Windows ***
-
-        --- Terminate connection
-        m.terminateConnection()
-
-        --- Get connected motor ids
-        m.getConnectIds()
-
-        ====== ID settings ======
-        --- Set new id to specified motor id
-        m.setNewIdOf(motor_id, new_id):
-
-        ====== Printing Information ======
-        --- All available baudrates
-        m.printAvailableBaudrates()
-
-        --- Motor status table
-        m.printControlTable(motor_id)
-
-        ====== Position Control ======
-        --- Get
-        m.getPositionOf(motor_id)
-        m.getPositionAll()
-
-        --- Set
-        m.setPositionOf(motor_id, pos=512, speed=90)
-        m.setPositionAll(pos=512, speed=90)
-
-        ====== Baudrate Control ======
-        --- Get
-        getBaudrateOf(motor_id)
-        getBaudrateAll()
-
-        --- Set
-        setBaudrateOf(motor_id, baudId=1)
-        setBaudrateAll(baudId=1)
-        '''
-
-        print(txt)
+            self.connection.setTorqueStatus(motorId, status)
